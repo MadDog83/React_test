bd3fc4a4b4a6e99d7bc8ca3325e3a7a573e28567
MadDog83 <130376768+MadDog83@users.noreply.github.com>
2023-12-03 16:49:21 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-/* import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode></React.StrictMode>);
- */
+/* const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode></React.StrictMode>); */
+
+/* const div = document.createElement('div'); */
+const root =document.getElementById('root');
+
+/* root.appendChild(div); */
+
+const div = React.createElement('div');
+ReactDOM.createRoot(root).render(div); 
